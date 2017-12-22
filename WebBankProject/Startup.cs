f7320d78cfda9e1e4e51e5c4eb8f1b4ec91ba974
stadio18 <stadio18@hotmail.com>
2017-12-22 09:28:19 +0200

    Dio Changes

--- WebBankProject/Startup.cs
+++ WebBankProject/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebBankProject.Models;
 
 namespace WebBankProject
 {
@@ -13,6 +15,8 @@ namespace WebBankProject
     {
         public Startup(IConfiguration configuration)
         {
+            //inject the file configuration appsettings.json
+            //This looking for configurations files here is appsettings.json
             Configuration = configuration;
         }
 
@@ -21,6 +25,14 @@ namespace WebBankProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //pass connection string from configuration file for dbcontext
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+            //Every time controller needs Iproductrepository we give him fakeproductrepository 
+            //so as to have some records to play.If later decide to use database we change fake 
+            //productrepository with database
+            //......services.AddTransient<IProductRepocitory, FakeProductRepository>();
+            //Add records from Database Entity framework
+            //services.AddTransient<IProductRepocitory, EfProductRepository>();
             services.AddMvc();
         }
 
